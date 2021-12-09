d0e2f7561f9c02a0d563621d2a3c6603bf439bba
“Arunesh <as8175802986@gmail.com>
2021-12-09 07:09:39 +0530

    [Arunesh]Added Code to File IO operations

---
+++ FileIoOperations-Architechture/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace FileIoOperations_Architechture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to File IO operations\n");
+            //FileExists();
+            //ReadAllLines();
+            //FIleCopy();
+            //FileDelete();
+            StreamReader ReadFromStreamReader = new StreamReader();
+            StreamReader.ReadFromStreamReader();
+            StreamReader.WriteFromStreamWriter();
+
+        }
+        public static void FileExists()
+        {
+            string path = @"E:\File IO\FileIO-Operations\FileIoOperations-Architechture\Sample.txt";
+
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+            }
+            Console.ReadKey();
+        }
+        public static void ReadAllLines()
+        {
+            string path = @"E:\File IO\FileIO-Operations\FileIoOperations-Architechture\Sample.txt";
+            string[] lines;
+            lines = File.ReadAllLines(path);
+
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+        }
+        public static void FIleCopy()
+        {
+            string path = @"E:\File IO\FileIO-Operations\FileIoOperations-Architechture\Sample.txt";
+            string copyPath = @"E:\File IO\FileIO-Operations\FileIoOperations-Architechture\Samplenew.txt";
+            File.Copy(path, copyPath);
+            Console.ReadKey();
+        }
+        public static void FileDelete()
+        {
+            string path = @"E:\File IO\FileIO-Operations\FileIoOperations-Architechture\Sample.txt";
+            File.Delete(path);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FileIoOperations-Architechture/StreamReader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileIoOperations_Architechture
+{
+    class StreamReader
+    {
+        public static void WriteFromStreamWriter()
+        {
+            string path = @"E:\File IO\FileIO-Operations\FileIoOperations-Architechture\Sample.txt";
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine("Hello my name is Arunesh");
+                sr.Close();
+                Console.WriteLine(File.ReadAllText(path));
+            }
+            Console.ReadKey();
+        }
+
+        public static void ReadFromStreamReader()
+        {
+            string path = @"E:\File IO\FileIO-Operations\FileIoOperations-Architechture\Sample.txt";
+            using (System.IO.StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
